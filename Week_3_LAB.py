7da868bccb331ad43f62f2f892e7dd8010c118c8
Gabriel Terci (s5625003) <s5625003@bournemouth.ac.uk>
2024-10-16 10:53:58 +0100

    Lab_3

---
+++ Week_3_LAB.py
@@ -0,0 +1,10 @@
+my_list = [15,23,32,46]
+print(my_list[0])
+print(my_list[-1])
+print(my_list[:2]) # or print(my_list[0:2])
+result = my_list[0] + my_list[-1] # add the first and last item and assign them to result
+my_list.append(result) # add result to the nd of the list using append method.
+
+print(result)
+
+##########################################################################################
